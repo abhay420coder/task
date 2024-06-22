9cd18f719f846a9e4cde70dcdca7080a8fc8c035
abhay420coder <ak8294836065@gmail.com>
2024-06-22 19:06:57 +0530

    - updated

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,7 +2,24 @@
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+			screens: {
+				'mobileLand': '576px', // mobile landscape
+				// => @media (min-width: 576px) { ... }
+		  
+				'tabletPot': '768px',
+				// => @media (min-width: 768px) { ... }
+		  
+				'tabletLand': '992px',
+				// => @media (min-width: 992px) { ... }
+
+				'laptop': '1200px',
+				// => @media (min-width: 120px) { ... }
+		  
+				'monitor': '1400px',
+				// => @media (min-width: 1400px) { ... }
+			  },
+		},
 	},
 	plugins: [],
 }
